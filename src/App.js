f097aff539083e7f12d583b5982f0ee33ac30528
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-09-05 16:57:29 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpendseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+      <h2>Let's get started!!!</h2>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
