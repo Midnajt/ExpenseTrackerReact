399bfb6d8de8e4221192faa3d9c60c10fa072faf
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-09-06 16:57:22 +0200

    ExpenseItem, css added, vars as text

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import ExpenseItem from "./components/ExpendseItem";
 function App() {
   return (
     <div>
-      <ExpenseItem></ExpenseItem>
       <h2>Let's get started!!!</h2>
-      <p>Hello world</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
