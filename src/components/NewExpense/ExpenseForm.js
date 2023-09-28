bee365b79befd402bc8c994ea00f80f6e62c0219
Marcin K <m.krzysztoszek@cermotor.com.pl>
2023-09-28 16:49:33 +0200

    alternative way to handle inputs events

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,35 +6,15 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  //   const [userInput, setUserInput] = useState({
-  //     enteredTitle: "",
-  //     enteredAmount: "",
-  //     enteredDate: "",
-  //   });
 
-  const titleChangeHandler = (e) => {
-    setEnteredTitle(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: e.target.value,
-    // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: e.target.value };
-    // });
-  };
-  const amountChangeHandler = (e) => {
-    setEnteredAmount(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: e.target.value,
-    // });
-  };
-  const dateChangeHandler = (e) => {
-    setEnteredDate(e.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: e.target.value,
-    // });
+  const inputChaneHandler = (identifier, value) => {
+    if (identifier === "title") {
+      setEnteredTitle(value);
+    } else if (identifier === "date") {
+      setEnteredDate(value);
+    } else {
+      setEnteredAmount(value);
+    }
   };
 
   return (
@@ -42,7 +22,10 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={(e) => inputChaneHandler("title", e.target.value)}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -50,7 +33,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={amountChangeHandler}
+            onChange={(e) => inputChaneHandler("amount", e.target.value)}
           />
         </div>
         <div className="new-expense__control">
@@ -59,7 +42,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2023-09-27"
-            onChange={dateChangeHandler}
+            onChange={(e) => inputChaneHandler("date", e.target.value)}
           />
         </div>
       </div>
